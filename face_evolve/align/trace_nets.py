006646ab1e9521df3564fa185ddafef2bd7ef6ed
miguel <miguel.varelaramos@braincreators.com>
2019-03-13 12:14:01 +0100

    output folder created for traced models

--- face_evolve/align/trace_nets.py
+++ face_evolve/align/trace_nets.py
@@ -1,15 +1,19 @@
+import os
+
 import torch
 from face_evolve.align.get_nets import PNet, RNet, ONet
 
 
 def main():
+    os.system("mkdir -p traced_models")
+
     print("Tracing PNet")
     pnet_input = torch.rand(1, 3, 224, 224)
     pnet = PNet()
     pnet.eval()
 
     traced_pnet = torch.jit.trace(pnet, pnet_input)
-    traced_pnet.save("pnet.pt")
+    traced_pnet.save("traced_models/pnet.pt")
 
     print("Tracing RNet")
     rnet_input = torch.rand(1, 3, 24, 24)
@@ -17,7 +21,7 @@ def main():
     rnet.eval()
 
     traced_pnet = torch.jit.trace(rnet, rnet_input)
-    traced_pnet.save("rnet.pt")
+    traced_pnet.save("traced_models/rnet.pt")
 
     print("Tracing ONet")
     onet_input = torch.rand(1, 3, 48, 48)
@@ -25,7 +29,7 @@ def main():
     onet.eval()
 
     traced_pnet = torch.jit.trace(onet, onet_input)
-    traced_pnet.save("onet.pt")
+    traced_pnet.save("traced_models/onet.pt")
 
 
 if __name__ == '__main__':
